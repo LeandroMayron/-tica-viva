4d24517373e8ae18eccf4296481030f12eba4111
Leandro Mayron <leandromayron9999@gmail.com>
2024-01-22 21:47:50 -0300

    (Style) Estilizado o header para tela de max-width: 551px

--- src/Componentes/Topo/index.js
+++ src/Componentes/Topo/index.js
@@ -5,7 +5,9 @@ export default function Topo() {
     return (
         <header className='limitarSecao'>
             <div className='topo'>
+                <div className='containerLogo'>
                 <img src={Logo} alt="Logo" />
+                </div>
                 <nav>
                     <a href="#produtos">PRODUTOS</a>
                     <a href="#sobre">SOBRE</a>
